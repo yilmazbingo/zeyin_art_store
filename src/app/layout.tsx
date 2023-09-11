cfb0f7d2427b41b9c3d815d0013e215bb63508d0
yilmaz <yilmazapis@gmail.com>
2023-09-10 23:19:55 -0400

    footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import "../styles/main.scss";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Footer from "@/components/ui/common/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Footer />
     </html>
   );
 }
